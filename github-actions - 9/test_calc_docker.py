92083c9737395e0257df0458e4cb6eed279438e1
eli <eli@eli.com>
2024-05-03 12:30:09 +0300

    GHA

---
+++ github-actions - 9/test_calc_docker.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+
+chrome_options = Options()
+options = [
+  "--headless",
+  "--disable-gpu",
+  "--window-size=1920,1200",
+  "--ignore-certificate-errors",
+  "--disable-extensions",
+  "--no-sandbox",
+  "--disable-dev-shm-usage"
+]
+
+for option in options:
+  chrome_options.add_argument(option)
+
+def test_site():
+  url = "http://localhost"
+  webdriver_path = 'C:\\Users\\eliey\\Desktop\\chromedriver_win32\\chromedriver.exe'
+
+  se = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+  driver = webdriver.Chrome(service=se,options=chrome_options)
+
+  driver.get(url)
+
+  btn1 = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[4]/div[1]/button").click()
+  btn_plus = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[4]/div[4]/button").click()
+  btn2 = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[4]/div[2]/button").click()
+  btn_eql = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[5]/div[3]/button").click()
+  result = driver.find_element(By.XPATH,"//*[@id='root']/div/div[1]/div").text
+
+  print(result)
+  driver.quit()
+  assert result == "3", "Wrong result"
