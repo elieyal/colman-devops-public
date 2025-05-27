b9f668ac3a8e688d4a90b2df3beb0c8d04a89410
Eli <eli@rescana.com>
2025-05-27 09:46:52 +0300

    class 8

---
+++ Selenium - 8/test_calc_headless.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+options.add_argument("no-sandbox")
+options.add_argument("headless")
+options.add_argument("window-size=1400,2100")
+
+def test_site():
+  url = "http://localhost"
+  #webdriver_path = 'C:\\Users\\eliey\\Desktop\\chromedriver_win32\\chromedriver.exe'
+
+  driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+  driver.get(url)
+
+
+  btn1 = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[4]/div[1]/button").click()
+  btn_plus = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[4]/div[4]/button").click()
+  btn2 = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[4]/div[2]/button").click()
+  btn_eql = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[5]/div[3]/button").click()
+  result = driver.find_element(By.XPATH,"//*[@id='root']/div/div[1]/div").text
+  print(result)
+  driver.quit()
+  assert result == "3", "Wrong result"
